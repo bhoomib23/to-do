1517dd5b8ec3e2af42f76f4af42e032d0857c870
Bhoomi <bhoomibassan941@gmail.com>
2026-06-13 02:38:05 +0530

    fix interface

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ function App() {
   const [todos, settodos] = useState([])
   const [showfinished, setshowfinished] = useState(true)
 
-  const savetols = params => {
-    localStorage.setItem("todos", JSON.stringify(params))
-  }
+  const savetols = (updatedTodos) => {
+  localStorage.setItem("todos", JSON.stringify(updatedTodos))
+}
 
-  const togglefinished = () => {
+  const togglefinished = (e) => {
   setshowfinished(!showfinished)
 }
 
@@ -42,7 +42,7 @@ function App() {
 
   const handledelete = (e, id) => {
     let newtodos = todos.filter(item => {
-      return item.id != id;
+      return item.id !== id;
     });
     settodos(newtodos);
     savetols(newtodos);
@@ -53,7 +53,7 @@ function App() {
   }
 
  const handleadd = () => {
-  let newtodos = [...todos, { id: uuidv4(), todo, iscompleted: false }];
+  let newtodos = [...todos, { id: uuidv4(), todo, isCompleted: false }];
   settodos(newtodos);
   settodo("");
   savetols(newtodos);   // <-- pass it, and compute it first
@@ -65,7 +65,7 @@ function App() {
       return item.id == id;
     })
     let newtodos = [...todos];
-    newtodos[index].iscompleted = !newtodos[index].iscompleted;
+    newtodos[index].isCompleted = !newtodos[index].isCompleted;
     settodos(newtodos);
     savetols(newtodos);
   }
@@ -74,24 +74,26 @@ function App() {
       <Navbar />
       <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-red-300 min-h-[80vh] md:w-[35%]">
       <h1 className="font-bold text-center text-2xl">iTask -Manage your tasks at one place</h1>
-        <div className="addtodo flex flex-col gap-4">
+        <div className="addtodo my-5 flex flex-col gap-4">
           <h2 className="text-lg font-bold">Add Todo</h2>
           <div className="flex">
           <input onChange={handlechange} value={todo} type="text" className="w-full rounded-lg px-5 py-1" />
-          <button onClick={handleadd} disabled={todo.length === 0} className="bg-red-200 hover:bg-r p-4 py-2 text-sm disabled:opacity-50 font-bold text-white rounded-full mx-2"> Save </button>
+          <button onClick={handleadd} disabled={todo.length === 0} className="bg-red-200 hover:bg-red-300  mx-2 p-4 py-2 text-sm disabled:opacity-50 font-bold text-white rounded-full"> Save </button>
           </div>
         </div>
-        <input className="my-4" type="checkbox" onChange={togglefinished} checked={showfinished} /> Show Finished
+        <input className='my-4' id='show' onChange={togglefinished} type="checkbox" checked={showfinished} /> 
+         <label className='mx-2' htmlFor="show">Show Finished</label> 
+         <div className='h-[1px] bg-black opacity-15 w-[90%] mx-auto my-2'></div>
         <h2 className="text-xl font-bold"> Your Todo List</h2>
         
         <div className="todos">
           {todos.length === 0 && <div className="m-5"> No Todos Added </div>}
           {todos.map(item => {
 
-            return (showfinished || !item.iscompleted) && <div key={item.id} className="todo flex justify-between my-3">
+            return (showfinished || !item.isCompleted) && <div key={item.id} className="todo flex my-3 justify-between">
               <div className="flex gap-5">
-                <input name={item.id} onChange={handlecheckbox} type="checkbox" checked={item.iscompleted} />
-                <div className={item.iscompleted ? "line-through" : ""}>
+                <input name={item.id} onChange={handlecheckbox} type="checkbox" checked={item.isCompleted} />
+                <div className={item.isCompleted ? "line-through" : ""}>
                   {item.todo}
                 </div>
               </div>
